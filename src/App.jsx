1a8c832cdfe53af2365b6ec2c58dd0c60b57bfdf
Emanuel Lopez <emanuel-lopez13@hotmail.com>
2023-08-07 21:28:55 -0300

    inicialización del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { NavBar } from "./components";
+import { Home } from "./pages/home";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Home  />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import './styles/navBar.css'
+
+
+const NavBar = () => {
+  return (
+    <nav className='container '>
+      hola
+    </nav>
+  )
+}
+
+export default NavBar
